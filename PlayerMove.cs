cbe48785b27d794c9d478aa79252239dbe874933
서연 <131230078+elregansekwon@users.noreply.github.com>
2023-12-11 11:55:18 +0900

    PlayerMove
    jump+좌우 이동+ 무한점프 막기

--- PlayerMove.cs
+++ PlayerMove.cs
@@ -9,33 +9,45 @@ public class PlayerMove : MonoBehaviour
     Rigidbody2D rigid;
     SpriteRenderer spriteRenderer;
     Animator anim;
-
+    bool isGrounded;
 
     void Awake()
     {
         rigid = GetComponent<Rigidbody2D>();
-        anim = GetComponent<Animator>();// anim 변수 초기화 추가
+        anim = GetComponent<Animator>();
     }
 
     void Update()
     {
-        
+        // Grounded 체크
+        isGrounded = Physics2D.Raycast(transform.position, Vector2.down, 0.1f, LayerMask.GetMask("Platform")).collider != null;
+
         //Jump
-        if (Input.GetButtonDown("Jump") && isGrounded){
-            rigid.AddForce(Vector2.up * jumpPower, ForceMode2D.Impulse);
-            anim.SetBool("isJumping", true);
+        if (Input.GetButtonDown("Jump"))
+        {
+            if (isGrounded) // 땅에 닿아 있는 경우에만 점프 가능
+            {
+                rigid.AddForce(Vector2.up * jumpPower, ForceMode2D.Impulse);
+                anim.SetBool("isJumping", true);
+            }
+            else // 땅에 닿아 있지 않은 경우에는 점프 불가
+            {
+                Debug.Log("점프할 수 없습니다. 땅에 착지해야 합니다.");
+            }
         }
+
         //Stop Speed
-        if (Input.GetButtonUp("Horizontal")) {
-            
+        if (Input.GetButtonUp("Horizontal"))
+        {
             rigid.velocity = new Vector2(rigid.velocity.normalized.x * 0.5f, rigid.velocity.y);
 
-        //Animation
-        if (Mathf.Abs(rigid.velocity.x) < 0.3)
-            anim.SetBool("isWalking", false);
-        else
-            anim.SetBool("isWalking", true);
+            //Animation
+            if (Mathf.Abs(rigid.velocity.x) < 0.3)
+                anim.SetBool("isWalking", false);
+            else
+                anim.SetBool("isWalking", true);
         }
+
         //Reset isJumping 파라미터
         if (isGrounded)
         {
@@ -43,8 +55,7 @@ public class PlayerMove : MonoBehaviour
         }
     }
 
-    
-    void FixedUpdate() 
+    void FixedUpdate()
     {
         //Move Speed
         float h = Input.GetAxisRaw("Horizontal");
@@ -55,25 +66,25 @@ public class PlayerMove : MonoBehaviour
         }
         else
         {
-        rigid.AddForce(Vector2.right * h, ForceMode2D.Impulse);
+            rigid.AddForce(Vector2.right * h, ForceMode2D.Impulse);
 
-        //Max Speed
-        if(rigid.velocity.x > maxSpeed) //Right Max Speed
-            rigid.velocity = new Vector2(maxSpeed, rigid.velocity.y);
-        else if(rigid.velocity.x < maxSpeed*(-1)) //Left Max Speed
-            rigid.velocity = new Vector2(maxSpeed*(-1), rigid.velocity.y);
+            //Max Speed
+            if (rigid.velocity.x > maxSpeed) //Right Max Speed
+                rigid.velocity = new Vector2(maxSpeed, rigid.velocity.y);
+            else if (rigid.velocity.x < maxSpeed * (-1)) //Left Max Speed
+                rigid.velocity = new Vector2(maxSpeed * (-1), rigid.velocity.y);
 
-        //Landing Platform
-        if(rigid.velocity.y < 0) {
-            Debug.DrawRay(rigid.position, Vector3.down, new Color(0,1,0));
-            RaycastHit2D rayHit = Physics2D.Raycast(rigid.position,Vector3.down, 1, LayerMask.GetMask("Platform"));
+            //Landing Platform
+            if (rigid.velocity.y < 0)
+            {
+                Debug.DrawRay(rigid.position, Vector3.down, new Color(0, 1, 0));
+                RaycastHit2D rayHit = Physics2D.Raycast(rigid.position, Vector3.down, 1, LayerMask.GetMask("Platform"));
 
-            if(rayHit.collider != null) {
-                if(rayHit.distance < 0.5f)
-                    anim.SetBool("isJumping",false);
-
+                if (rayHit.collider != null)
+                {
+                    if (rayHit.distance < 0.5f)
+                        anim.SetBool("isJumping", false);
                 }
-        
             }
         }
     }
